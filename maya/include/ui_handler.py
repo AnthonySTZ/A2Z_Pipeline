90f426727ba3b58d98ee4d5a2e91fdc47589ec2a
AnthonySTZ <anthonycandy2309@gmail.com>
2024-07-28 14:35:47 +0200

    init ui file for maya window

--- maya/include/ui_handler.py
+++ maya/include/ui_handler.py
@@ -1,10 +1,26 @@
-from PySide2.QtWidgets import QMainWindow
+import os
+from PySide2.QtWidgets import QMainWindow, QVBoxLayout, QApplication
+from PySide2 import QtCore, QtUiTools
 
 
 class MainWindow(QMainWindow):
-    def __init__(self, parent=None):
+    def __init__(self, parent=QApplication.activeWindow()):
         super().__init__(parent)
 
+        self.init_maya_ui("interface\\save_as.ui")
+
     def show_window(self) -> None:
-        self.resize(890, 550)
+        self.resize(896, 282)
         self.show()
+
+    def init_maya_ui(self, uiRelativePath) -> None:
+        loader = QtUiTools.QUiLoader()
+        dirname = os.path.dirname(__file__)
+        uiFilePath = os.path.join(dirname, uiRelativePath)
+        uifile = QtCore.QFile(uiFilePath)
+        uifile.open(QtCore.QFile.ReadOnly)
+        print(uiFilePath)
+        self.ui = loader.load(uifile)
+        self.centralLayout = QVBoxLayout(self)
+        self.centralLayout.setContentsMargins(0, 0, 0, 0)
+        self.centralLayout.addWidget(self.ui)
