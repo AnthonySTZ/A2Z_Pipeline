74c97441d44bbb2ce1367c09874b3e98bbaa9c92
AnthonySTZ <anthonycandy2309@gmail.com>
2024-07-29 14:20:06 +0200

    change project handling in maya script

--- maya/include/ui_handler.py
+++ maya/include/ui_handler.py
@@ -1,6 +1,7 @@
 import os
 from PySide2.QtWidgets import QDialog, QVBoxLayout, QApplication
 from PySide2 import QtCore, QtUiTools
+from include.project_handler import ProjectHandler
 
 PROJECT_PATH = "A:/Programming/A2Z_Pipeline/test"
 
@@ -11,6 +12,7 @@ class MainWindow(QDialog):
 
         self.init_maya_ui("interface\\save_as.ui")
         self.projects_path = PROJECT_PATH
+        self.project = None
 
     def show_window(self) -> None:
         self.resize(798, 161)
@@ -19,10 +21,12 @@ class MainWindow(QDialog):
 
     def init_ui(self) -> None:
         self.update_projects_list()
+        self.selected_project_changed()
         self.update_shots_list()
         self.update_kind_list()
-        self.ui.cb_project.currentIndexChanged.connect(self.update_shots_list)
+        self.ui.cb_project.currentIndexChanged.connect(self.selected_project_changed)
         self.ui.cb_type.currentIndexChanged.connect(self.update_kind_list)
+        self.ui.cb_kind.currentIndexChanged.connect(self.update_path)
 
     def init_maya_ui(self, uiRelativePath) -> None:
         loader = QtUiTools.QUiLoader()
@@ -49,23 +53,31 @@ class MainWindow(QDialog):
         for project_name in project_names:
             self.ui.cb_project.addItem(project_name)
 
-    def update_shots_list(self) -> None:
-        """Populate shot list combo box with available shots from the selected project"""
+        self.update_path()
+
+    def selected_project_changed(self) -> None:
+        """Update save path label with the selected project, shot, and kind"""
         selected_project = self.ui.cb_project.currentText()
         if selected_project == "":
+            self.project = None
             return
         project_path = os.path.join(self.projects_path, selected_project)
         if not os.path.exists(project_path):
             print(f"Project '{selected_project}' not found")
             return
-        shots = [
-            shot.name
-            for shot in os.scandir(os.path.join(project_path, "40_shots"))
-            if shot.is_dir()
-        ]
+        self.project = ProjectHandler(project_path)
+        self.update_shots_list()
+
+    def update_shots_list(self) -> None:
+        """Populate shot list combo box with available shots from the selected project"""
+        if self.project is None:
+            return
+        shots = self.project.get_all_shots()
         self.ui.cb_shot.clear()
         for shot in shots:
-            self.ui.cb_shot.addItem(shot)
+            self.ui.cb_shot.addItem("s" + shot.number + "_" + shot.name)
+
+        self.update_path()
 
     def update_kind_list(self) -> None:
         """Populate kind list combo box with available kinds"""
@@ -78,3 +90,15 @@ class MainWindow(QDialog):
         type = self.ui.cb_type.currentText()
         for kind in kinds[type]:
             self.ui.cb_kind.addItem(kind)
+
+        self.update_path()
+
+    def update_path(self) -> None:
+        """Update save path label with the selected project, shot, and kind"""
+        selected_shot = self.ui.cb_shot.currentText()
+        selected_kind = self.ui.cb_kind.currentText()
+        if self.project is None or selected_shot == "" or selected_kind == "":
+            self.ui.l_path.setText("")
+            return
+        self.scene_name = selected_shot + "_" + selected_kind + "_v001.mb"
+        self.ui.l_path.setText(self.scene_name)
